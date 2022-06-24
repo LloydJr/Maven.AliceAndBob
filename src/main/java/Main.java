a3f1340d75feccb3c0969a8288195a57e79d9efb
Lloyd <lloyd@zipcodersmbp68.lan>
2022-06-23 21:56:00 -0400

    Finished the Lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,24 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
+
 public class Main {
+	static String name = "";
+    public static void main(String[] args ) {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter your name: ");
+		String name = in.nextLine();
 
-    public static void main(String[] args ){
-
+	if (name.equals("Alice")) {
+		System.out.println("Alice");
+	}
+	else if (name.equals("Bob")) {
+		System.out.println("Bob");
+	}
+	else {
+		System.out.println("Invalid");
+	}
     }
 }
